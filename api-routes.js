ef41fa249a0271bb51d9b2be3dadb0329caa3a01
Brian McMillan <brianm_91@hotmail.com>
2016-11-08 13:28:18 -0500

    Uploading base explorer mode

---
+++ api-routes.js
@@ -0,0 +1,20 @@
+// Libraries
+var express = require('express')
+var router = express.Router()
+
+// SQLite3 Library (https://github.com/mapbox/node-sqlite3/wiki/API)
+var sqlite3 = require('sqlite3').verbose()
+var db = new sqlite3.Database('./store.sqlite3')
+
+// Routes
+router.get('/users', function (req, res) {
+  db.serialize(function () {
+
+    // Your db code goes here, see SQLite3 library docs...
+    db.all('SELECT * FROM users', (error, rows) => {
+            res.json(rows)
+        })
+  })
+})
+
+module.exports = router
